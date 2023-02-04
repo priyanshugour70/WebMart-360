777a3119ea0126aa8268058d41dba9316c0e754f
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-05 01:23:46 +0530

    Completed Footer Top

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Banner from './components/home/Banner';
 
@@ -7,6 +8,7 @@ function App() {
     <div className='font-bodyFont'>
     <Header />
     <Banner />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/footer/FooterMiddle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FooterMiddle() {
+  return (
+    <div>FooterMiddle</div>
+  )
+}
+
+export default FooterMiddle
