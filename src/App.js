4a2671d7ae7aa22f6727bd4eafec52dbd67e5306
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-05 04:04:52 +0530

    React Router Dom SetUp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Banner from './components/home/Banner';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+
+
+const Layout =()=>{
+  return(
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+
+    <Route path='/' element={<Layout />}>
+    <Route index element={<Home />}></Route>
+    </Route>
+  ))
+
   return (
     <div className='font-bodyFont'>
-    <Header />
-    <Banner />
-    <Footer />
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
