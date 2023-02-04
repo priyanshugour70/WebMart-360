5a7b95786e64c1de4bc03912bf87763c5c0ab87f
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-04 16:17:21 +0530

    Added react-slick package

---
+++ src/components/home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Banner() {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      
+  return (
+    <div>Banner</div>
+  )
+}
+
+export default Banner
