3e33480d25a9e7640979ce7746082d81c6ea269b
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-04 16:27:26 +0530

    Start making Banner

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import {
+    bannerImgOne,
+    bannerImgTwo,
+    bannerImgThree,
+    bannerImgFour,
+    bannerImgFive,
+} from "../../assets";
 
 function Banner() {
-
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        autoplay: true,
         slidesToShow: 1,
-        slidesToScroll: 1
-      };
-      
-  return (
-    <div>Banner</div>
-  )
+        slidesToScroll: 1,
+        arrows: false,
+    };
+
+    return (
+        <div>
+            <Slider {...settings}>
+                <div>
+                    <img src={bannerImgOne} alt="bannerImgOne" />
+                </div>
+                <div>
+                    <img src={bannerImgTwo} alt="bannerImgTwo" />
+                </div>
+                <div>
+                    <img src={bannerImgThree} alt="bannerImgThree" />
+                </div>
+                <div>
+                    <img src={bannerImgFour} alt="bannerImgFour" />
+                </div>
+                <div>
+                    <img src={bannerImgFive} alt="bannerImgFive" />
+                </div>
+            </Slider>
+        </div>
+    );
 }
 
-export default Banner
+export default Banner;
